e23682a78884a21368d3a9b4f0471197d3e033dc
Luigi Di Sotto <luigi.disotto@aitechnologies.it>
2020-08-30 12:45:13 +0200

    fix indent using c/c++ vscode formatter

--- src/nq-bitpattern-ff.cpp
+++ src/nq-bitpattern-ff.cpp
@@ -4,20 +4,20 @@
 #include <math.h>
 #include <stdlib.h>
 
-
 using namespace std;
 using namespace ff;
 
 int mask = 0;
 
-typedef struct {
-    int columns;
-    int rightDiagonal;
-    int leftDiagonal;
+typedef struct
+{
+	int columns;
+	int rightDiagonal;
+	int leftDiagonal;
 } ff_task_t;
 
-
-class Emitter: public ff_node {
+class Emitter : public ff_node
+{
 
 public:
 	int boardsize;
@@ -26,21 +26,30 @@ public:
 	double emitterTsynch;
 	double Te;
 
-public:	
+public:
+	Emitter(int n, int d)
+	{
+		boardsize = n;
+		depth = d;
+		numberOfTasks = 0;
+		emitterTsynch = 0;
+		Te = 0;
+	}
 
-	Emitter(int n, int d){ boardsize = n; depth = d; numberOfTasks = 0;  emitterTsynch = 0; Te = 0;}
-
-	int svc_init() {
+	int svc_init()
+	{
 		mask = (1 << boardsize) - 1;
 		return 0;
 	}
 
-	inline void generateTree(int leftDiagonal, int columns, int rightDiagonal, int actualRow){
+	inline void generateTree(int leftDiagonal, int columns, int rightDiagonal, int actualRow)
+	{
 
-		if(actualRow == depth-1){
-			ff_task_t * task = (ff_task_t*)malloc(sizeof(ff_task_t));
-			task->columns       = columns;
-			task->leftDiagonal  = leftDiagonal;
+		if (actualRow == depth - 1)
+		{
+			ff_task_t *task = (ff_task_t *)malloc(sizeof(ff_task_t));
+			task->columns = columns;
+			task->leftDiagonal = leftDiagonal;
 			task->rightDiagonal = rightDiagonal;
 
 			auto start = std::chrono::high_resolution_clock::now();
@@ -56,30 +65,32 @@ public:
 
 		register int rowFreePositions = ~(leftDiagonal | columns | rightDiagonal) & mask;
 
-		while(rowFreePositions){
-		   register int lsb = rowFreePositions & -rowFreePositions;
-		   rowFreePositions -= lsb;
-		   int n = actualRow + 1;
-		   generateTree( (leftDiagonal|lsb)<<1, (columns|lsb), (rightDiagonal|lsb)>>1, n);
+		while (rowFreePositions)
+		{
+			register int lsb = rowFreePositions & -rowFreePositions;
+			rowFreePositions -= lsb;
+			int n = actualRow + 1;
+			generateTree((leftDiagonal | lsb) << 1, (columns | lsb), (rightDiagonal | lsb) >> 1, n);
 		}
-
 	}
 
-	 void * svc(void * t) {
-	 	auto start = std::chrono::high_resolution_clock::now();
-	 	generateTree(0, 0, 0, 0);
-	 	auto end = std::chrono::high_resolution_clock::now();
+	void *svc(void *t)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		generateTree(0, 0, 0, 0);
+		auto end = std::chrono::high_resolution_clock::now();
 
-	 	Te = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()/1000;
-	 	Te = Te/numberOfTasks;
+		Te = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000;
+		Te = Te / numberOfTasks;
 
-	 	emitterTsynch = emitterTsynch/1000;
+		emitterTsynch = emitterTsynch / 1000;
 
-	 	return NULL;
-	 }
-};	
+		return NULL;
+	}
+};
 
-class Worker: public ff_node {
+class Worker : public ff_node
+{
 
 public:
 	unsigned long int workerSolutions;
@@ -87,60 +98,61 @@ public:
 	unsigned long int numberOfTasks;
 
 public:
+	void _try(int leftDiagonal, int columns, int rightDiagonal)
+	{
 
-	void _try(int leftDiagonal, int columns, int rightDiagonal){
+		register int _leftDiagonal = leftDiagonal;
+		register int _columns = columns;
+		register int _rightDiagonal = rightDiagonal;
 
-		register int _leftDiagonal    = leftDiagonal;
-		register int _columns 		  = columns;
-		register int _rightDiagonal   = rightDiagonal;
-
-		if(columns == mask){
+		if (columns == mask)
+		{
 			workerSolutions++;
 		}
 
 		register int rowFreePositions = ~(_leftDiagonal | _columns | _rightDiagonal) & mask;
 
-		while(rowFreePositions){  
-		   register int lsb = rowFreePositions & -rowFreePositions;
-		   rowFreePositions -= lsb;
-		   _try( (_leftDiagonal|lsb)<<1, (_columns|lsb), (_rightDiagonal|lsb)>>1 );
+		while (rowFreePositions)
+		{
+			register int lsb = rowFreePositions & -rowFreePositions;
+			rowFreePositions -= lsb;
+			_try((_leftDiagonal | lsb) << 1, (_columns | lsb), (_rightDiagonal | lsb) >> 1);
+		}
+	}
+
+	Worker() { workerSolutions = 0; }
+
+	int getSolutions() { return workerSolutions; }
+
+	void *svc(void *t)
+	{
+
+		auto start = std::chrono::high_resolution_clock::now();
+		ff_task_t *task = (ff_task_t *)t;
+		if (task)
+		{
+			numberOfTasks++;
 		}
 
+		int leftDiagonal = task->leftDiagonal,
+			columns = task->columns,
+			rightDiagonal = task->rightDiagonal;
+
+		_try(leftDiagonal, columns, rightDiagonal);
+
+		free(task);
+
+		auto end = std::chrono::high_resolution_clock::now();
+		Tw += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000;
+
+		//unsigned long int * solutions = (unsigned long int *)malloc(sizeof(unsigned long int));
+		//*solutions = workerSolutions;
+		//ff_send_out(solutions);
+		//workerSolutions = 0;
+
+		return GO_ON;
 	}
-	
-	 Worker(){ workerSolutions = 0; }
-
-	 int getSolutions(){ return workerSolutions; }
-
-	 void * svc(void * t) {
-
-	 	auto start = std::chrono::high_resolution_clock::now();
-	 	ff_task_t * task  = (ff_task_t *)t;
-	 	if(task){
-	 	  numberOfTasks++;
-	 	}
-	 	
-	 	int leftDiagonal  = task->leftDiagonal,
-	 		columns		  = task->columns,
-	 		rightDiagonal = task->rightDiagonal;
-
-	 	_try(leftDiagonal, columns, rightDiagonal);
-
-	 	free(task);
-
-	 	auto end = std::chrono::high_resolution_clock::now();
-	 	Tw += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()/1000;
-
-	 	//unsigned long int * solutions = (unsigned long int *)malloc(sizeof(unsigned long int));
-	 	//*solutions = workerSolutions;	 	
-	 	//ff_send_out(solutions);
-	 	//workerSolutions = 0;
-
-        return GO_ON;
-    }
-
 };
-
 
 /*class Collector: public ff_node {
 
@@ -175,13 +187,14 @@ public:
 
 };*/
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
 	int n = atoi(argv[1]),
 		nw = atoi(argv[2]),
 		d = atoi(argv[3]);
 
-	unsigned long int numberOfSolutions = 0;	
+	unsigned long int numberOfSolutions = 0;
 
 	ff_farm<> farm;
 
@@ -189,33 +202,33 @@ int main(int argc, char* argv[]) {
 	farm.add_emitter(&E);
 
 	vector<ff_node *> w;
-    for(int i=0; i<nw; i++) 
-    	w.push_back(new Worker());
-    	
-    farm.add_workers(w);	
-	
+	for (int i = 0; i < nw; i++)
+		w.push_back(new Worker());
+
+	farm.add_workers(w);
+
 	//Collector C(nw);
 	//farm.add_collector(&C);
 
-	if (farm.run_and_wait_end()<0) {
-        error("running farm\n");
-        return -1;
-    }
+	if (farm.run_and_wait_end() < 0)
+	{
+		error("running farm\n");
+		return -1;
+	}
 
-    //reduce solutions
-    auto start = std::chrono::high_resolution_clock::now();
+	//reduce solutions
+	auto start = std::chrono::high_resolution_clock::now();
 
-	for(int i=0; i<nw; i++) 
-    	numberOfSolutions += ((Worker *)w[i])->getSolutions();
+	for (int i = 0; i < nw; i++)
+		numberOfSolutions += ((Worker *)w[i])->getSolutions();
 
-    auto end = std::chrono::high_resolution_clock::now();
+	auto end = std::chrono::high_resolution_clock::now();
 	double emitter_ts = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
-    //farm.ffStats(std::cout);
+	//farm.ffStats(std::cout);
 
-    cout << endl;
-    cout << "n. sol = " << numberOfSolutions << endl;
-    cout << "farm completion time = " << farm.ffTime() << " (ms)" << endl;
-    cout << endl;
-
+	cout << endl;
+	cout << "n. sol = " << numberOfSolutions << endl;
+	cout << "farm completion time = " << farm.ffTime() << " (ms)" << endl;
+	cout << endl;
 }
--- src/nq-serial.cpp
+++ src/nq-serial.cpp
@@ -6,37 +6,39 @@ using namespace std;
 int mask = 0;
 int count = 0;
 
-inline void _try(int ld, int cols, int rd){
+inline void _try(int ld, int cols, int rd)
+{
 
-	if(cols == mask){
+	if (cols == mask)
+	{
 		count++;
 	}
 
 	register int poss = ~(ld | cols | rd) & mask;
 
-	while(poss){
-	   register int lsb = poss & -poss;
-	   poss -= lsb;
-	   _try( (ld|lsb)<<1, (cols|lsb), (rd|lsb)>>1);
+	while (poss)
+	{
+		register int lsb = poss & -poss;
+		poss -= lsb;
+		_try((ld | lsb) << 1, (cols | lsb), (rd | lsb) >> 1);
 	}
-
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
 	int n = atoi(argv[1]);
 	mask = (1 << n) - 1;
 
 	auto start = std::chrono::high_resolution_clock::now();
-	_try(0,0,0);
+	_try(0, 0, 0);
 	auto end = std::chrono::high_resolution_clock::now();
 
 	double t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	
+
 	std::cout << "completion time: " << t << " (ms)" << std::endl;
 	//std::cout << t << std::endl;
 	//std::cout << "n. solutions: " << count << std::endl;
 
 	return 0;
-
 }

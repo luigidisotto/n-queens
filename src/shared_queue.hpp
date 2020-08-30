e3a4e4aaf065e15486bc269a59006f61c089f1f9
Luigi Di Sotto <luigi.disotto@aitechnologies.it>
2020-08-30 12:42:08 +0200

    fix c++ code indent with vscode

--- src/shared_queue.hpp
+++ src/shared_queue.hpp
@@ -2,63 +2,74 @@
 #define __SHARED_QUEUE__
 
 #include <pthread.h>
-#include <queue> 
+#include <queue>
 
 using namespace std;
 
-template <typename T> class SharedQueue{
-	
-	public:
-		queue<T> queue;
-		unsigned long int maxSize;
-		pthread_mutex_t mutex;
-		pthread_cond_t cond;
-		
-	public:
+template <typename T>
+class SharedQueue
+{
 
-		SharedQueue(){
-			pthread_mutex_init(&mutex, NULL);
-			pthread_cond_init(&cond, NULL);
-			maxSize = -1; //infinite capacity
+public:
+	queue<T> queue;
+	unsigned long int maxSize;
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+
+public:
+	SharedQueue()
+	{
+		pthread_mutex_init(&mutex, NULL);
+		pthread_cond_init(&cond, NULL);
+		maxSize = -1; //infinite capacity
+	}
+
+	SharedQueue(unsigned long int max)
+	{
+		pthread_mutex_init(&mutex, NULL);
+		pthread_cond_init(&cond, NULL);
+		maxSize = max;
+	}
+
+	~SharedQueue()
+	{
+		pthread_mutex_destroy(&mutex);
+		pthread_cond_destroy(&cond);
+	}
+
+	inline void push(T e)
+	{
+		pthread_mutex_lock(&mutex);
+		while (queue.size() == maxSize)
+		{
+			pthread_cond_wait(&cond, &mutex);
 		}
+		queue.push(e);
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_signal(&cond); //by signaling here we reduce locking overhead
+	}
 
-		SharedQueue(unsigned long int max){
-			pthread_mutex_init(&mutex, NULL);
-			pthread_cond_init(&cond, NULL);
-			maxSize = max;
+	inline T pop()
+	{
+		pthread_mutex_lock(&mutex);
+		while (queue.size() == 0)
+		{
+			pthread_cond_wait(&cond, &mutex);
 		}
+		T e = queue.front();
+		queue.pop();
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_signal(&cond);
+		return e;
+	}
 
-
-		~SharedQueue() {
-			pthread_mutex_destroy(&mutex);
-			pthread_cond_destroy(&cond);
-		}
-
-		inline void push(T e){
-			pthread_mutex_lock(&mutex);
-			while (queue.size() == maxSize) { pthread_cond_wait(&cond, &mutex); }
-			queue.push(e);
-			pthread_mutex_unlock(&mutex);
-			pthread_cond_signal(&cond); //by signaling here we reduce locking overhead
-		}
-
-		inline T pop(){
-			pthread_mutex_lock(&mutex);
-			while (queue.size() == 0) { pthread_cond_wait(&cond, &mutex); }
-			T e = queue.front();
-			queue.pop();
-			pthread_mutex_unlock(&mutex);
-			pthread_cond_signal(&cond);
-			return e;
-		}
-		
-		inline int size() {
-			pthread_mutex_lock(&mutex);
-			int size = queue.size();
-			pthread_mutex_unlock(&mutex);
-			return size;
-		}
-
+	inline int size()
+	{
+		pthread_mutex_lock(&mutex);
+		int size = queue.size();
+		pthread_mutex_unlock(&mutex);
+		return size;
+	}
 };
 
 #endif
